a6ea16e5c4876183b42d83528e7ae83b1db6263c
Botherstm <jamesloro12012002@gmail.com>
2022-10-04 10:43:39 +0800

    ketujuh commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,37 +4,37 @@ import 'package:tugas1/home.dart';
 
 import 'pages/login.dart';
 
-void main() {
-  runApp(Home());
-}
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: const FirebaseOptions(
-//         apiKey: "AIzaSyDw4VnlvNYVsc1jB_PJcj6zxWjwRtOjHGw",
-//         authDomain: "aplikasi-koperasi-eeb64.firebaseapp.com",
-//         projectId: "aplikasi-koperasi-eeb64",
-//         storageBucket: "aplikasi-koperasi-eeb64.appspot.com",
-//         messagingSenderId: "876743140978",
-//         appId: "1:876743140978:web:840f23ceda85345eb344eb"),
-//   );
-
-//   runApp(
-//     const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Check(),
-//       // home: FutureBuilder(
-//       //   builder: (context, snapshot) {
-//       //     if (snapshot.connectionState != ConnectionState.done) {
-//       //       return const Home();
-//       //     }
-//       //     return const Login();
-//       //   },
-//       // ),
-//     ),
-//   );
+// void main() {
+//   runApp(Home());
 // }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyDw4VnlvNYVsc1jB_PJcj6zxWjwRtOjHGw",
+        authDomain: "aplikasi-koperasi-eeb64.firebaseapp.com",
+        projectId: "aplikasi-koperasi-eeb64",
+        storageBucket: "aplikasi-koperasi-eeb64.appspot.com",
+        messagingSenderId: "876743140978",
+        appId: "1:876743140978:web:840f23ceda85345eb344eb"),
+  );
+
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Check(),
+      // home: FutureBuilder(
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState != ConnectionState.done) {
+      //       return const Home();
+      //     }
+      //     return const Login();
+      //   },
+      // ),
+    ),
+  );
+}
 
 class Check extends StatefulWidget {
   const Check({super.key});
