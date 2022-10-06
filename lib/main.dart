750b3dbf53875968780c38c8f4cebeed87790d48
Botherstm <jamesloro12012002@gmail.com>
2022-10-07 01:56:20 +0800

    kesepuluh commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,49 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tugas1/home.dart';
 
 import 'pages/login.dart';
 
-// void main() {
-//   runApp(Home());
-// }
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyDw4VnlvNYVsc1jB_PJcj6zxWjwRtOjHGw",
-        authDomain: "aplikasi-koperasi-eeb64.firebaseapp.com",
-        projectId: "aplikasi-koperasi-eeb64",
-        storageBucket: "aplikasi-koperasi-eeb64.appspot.com",
-        messagingSenderId: "876743140978",
-        appId: "1:876743140978:web:840f23ceda85345eb344eb"),
-  );
-
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Check(),
-      // home: FutureBuilder(
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState != ConnectionState.done) {
-      //       return const Home();
-      //     }
-      //     return const Login();
-      //   },
-      // ),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
-class Check extends StatefulWidget {
-  const Check({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<Check> createState() => _CheckState();
-}
-
-class _CheckState extends State<Check> {
-  final _initizilitation = Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyDw4VnlvNYVsc1jB_PJcj6zxWjwRtOjHGw",
-        authDomain: "aplikasi-koperasi-eeb64.firebaseapp.com",
-        projectId: "aplikasi-koperasi-eeb64",
-        storageBucket: "aplikasi-koperasi-eeb64.appspot.com",
-        messagingSenderId: "876743140978",
-        appId: "1:876743140978:web:840f23ceda85345eb344eb"),
-  );
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initizilitation,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return Home();
-        }
-        return const Login();
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'aplikasi opesrasi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Login(),
     );
   }
 }
+
+// ignore: must_be_immutable
+
+// ignore: must_be_immutable
+// class Checker extends StatelessWidget {
+//   var password;
+
+//   var username;
+//   Checker({super.key, required this.username, required this.password});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder(
+//       builder: (context, snapshot) {
+//         if (ConnectionState == ConnectionState.done) {
+//           return Home(
+//             password: null,
+//             username: null,
+//           );
+//         }
+//         return const Login();
+//       },
+//     );
+//   }
+// }
